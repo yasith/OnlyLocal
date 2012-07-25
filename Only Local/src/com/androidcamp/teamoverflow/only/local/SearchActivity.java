04f249eaf699b2fbe7bbd163bceec6af816aad42
Yasith Vidanaarachchi <yasith.vidanaarachchi@gmail.com>
2012-07-25 14:05:39 -0700

    Clean up SearchActivity

--- Only Local/src/com/androidcamp/teamoverflow/only/local/SearchActivity.java
+++ Only Local/src/com/androidcamp/teamoverflow/only/local/SearchActivity.java
@@ -1,37 +1,13 @@
 package com.androidcamp.teamoverflow.only.local;
 
-import com.androidcamp.teamoverflow.only.local.R;
-
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
-public class SearchActivity extends Activity {
+public class SearchActivity extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_search, menu);
-        return true;
-    }
-
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
