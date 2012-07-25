787b60b625be119ed34945663548ef062bcef6c6
Yasith Vidanaarachchi <yasith.vidanaarachchi@gmail.com>
2012-07-25 14:48:29 -0700

    Add list items to the list view

--- Only Local/src/com/androidcamp/teamoverflow/only/local/SearchActivity.java
+++ Only Local/src/com/androidcamp/teamoverflow/only/local/SearchActivity.java
@@ -1,6 +1,11 @@
 package com.androidcamp.teamoverflow.only.local;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.app.ListActivity;
 
 public class SearchActivity extends ListActivity {
@@ -11,5 +16,13 @@ public class SearchActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_search);
+        
+        List<String> markets = new ArrayList<String>();
+        
+        for (int i = 0; i < 200; i++) {
+        	markets.add("Market " + (i+1));
+		}
+        
+        this.setListAdapter(new ArrayAdapter(this, R.layout.search_item, R.id.market_name, markets));
     }
 }
