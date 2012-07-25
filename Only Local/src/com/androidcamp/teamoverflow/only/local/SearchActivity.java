ac46def71f304ae954674472c256cab08472b5a0
Yasith Vidanaarachchi <yasith.vidanaarachchi@gmail.com>
2012-07-25 14:17:59 -0700

    Add no data part to the SearchActivity

--- Only Local/src/com/androidcamp/teamoverflow/only/local/SearchActivity.java
+++ Only Local/src/com/androidcamp/teamoverflow/only/local/SearchActivity.java
@@ -9,5 +9,7 @@ public class SearchActivity extends ListActivity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_search);
     }
 }
