78dcb594d2bfd87f7f9779d2dd83bc8791bc5c37
unknown <zoruiz24@gmail.com>
2012-07-24 22:11:23 -0700

    Change to StartActivity

--- Only Local/gen/com/example/only/local/R.java
+++ Only Local/gen/com/example/only/local/R.java
@@ -15,7 +15,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int TextView01=0x7f070003;
+        public static final int TextView02=0x7f070002;
+        public static final int editText1=0x7f070001;
+        public static final int menu_settings=0x7f070004;
+        public static final int textView1=0x7f070000;
     }
     public static final class layout {
         public static final int activity_start=0x7f030000;
@@ -25,8 +29,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int change=0x7f040006;
         public static final int hello_world=0x7f040001;
+        public static final int loc=0x7f040005;
         public static final int menu_settings=0x7f040002;
+        public static final int search=0x7f040004;
         public static final int title_activity_start=0x7f040003;
     }
     public static final class style {
--- Only Local/src/com/example/only/local/StartActivity.java
+++ Only Local/src/com/example/only/local/StartActivity.java
@@ -1,7 +1,11 @@
 package com.example.only.local;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 
 public class StartActivity extends Activity {
@@ -10,6 +14,13 @@ public class StartActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        LocationManager locman = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locman.getLastKnownLocation("GPS");
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        Log.d("LOCATION TEST", latitude + " " + longitude);
+        
+        
     }
 
     @Override
