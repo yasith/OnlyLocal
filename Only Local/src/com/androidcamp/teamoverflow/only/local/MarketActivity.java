768adbe67e1dabdc8d0fdbbd29bc5f1eb4ace7da
unknown <zoruiz24@gmail.com>
2012-07-26 23:17:47 -0700

    adding intent to handle maps

--- Only Local/src/com/androidcamp/teamoverflow/only/local/MarketActivity.java
+++ Only Local/src/com/androidcamp/teamoverflow/only/local/MarketActivity.java
@@ -1,14 +1,20 @@
 package com.androidcamp.teamoverflow.only.local;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MarketActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
+		super.onCreate(savedInstanceState);	
 		setContentView(R.layout.activity_market);
+	}
+	
+	public void sendToMaps(String address){
+		Uri location = Uri.parse("geo:0,0?q=address");
+		Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);	
 	}
 }
