0182a3cfe99e6d807cb43246aba17ecde55ef83c
Yasith Vidanaarachchi <yasith.vidanaarachchi@gmail.com>
2012-07-26 17:31:11 -0700

    Add two rows to search results

--- Only Local/src/com/androidcamp/teamoverflow/only/local/SearchActivity.java
+++ Only Local/src/com/androidcamp/teamoverflow/only/local/SearchActivity.java
@@ -7,6 +7,7 @@ import com.androidcamp.teamoverflow.only.local.PlacesService.SearchAsyncCallback
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -39,24 +40,27 @@ public class SearchActivity extends ListActivity implements SearchAsyncCallback 
 			TextView emptyText = (TextView) findViewById(android.R.id.empty);
 			emptyText.setText("No Results Found");
 		}
-//		
-//        ListView ls = (ListView)findViewById(android.R.id.list);
-//        ls.setOnItemClickListener(new OnItemClickListener() {
-//                @Override
-//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                        // @todo
-//                }
-//        });	
+		
+        ListView ls = (ListView)findViewById(android.R.id.list);
+        ls.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+					Log.d(TAG, "Position " + position);
+				}
+        });	
 	}
 
 	@Override
 	public void onSearchCompleted(ArrayList<Place> marketPlaces) {
 		List<String> markets = new ArrayList<String>();
+		List<String> vicinities = new ArrayList<String>();
 
 		Log.d(TAG, "Loaded " + markets.size() + " results");
 
 		for (int i = 0; i < marketPlaces.size(); i++) {
 			markets.add(marketPlaces.get(i).name);
+//			vicinities.add(marketPlaces.get(i).vicinity);
 		}
 		this.setListAdapter(new ArrayAdapter(this, R.layout.search_item,
 				R.id.market_name, markets));
