b079dbb21546ef231a629f7bd22be72521b27d56
Yasith Vidanaarachchi <yasith.vidanaarachchi@gmail.com>
2012-07-26 22:09:47 -0700

    Add Market Description Screen

--- Only Local/src/com/androidcamp/teamoverflow/only/local/MarketActivity.java
+++ Only Local/src/com/androidcamp/teamoverflow/only/local/MarketActivity.java
@@ -1,7 +1,14 @@
 package com.androidcamp.teamoverflow.only.local;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MarketActivity extends Activity {
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_market);
+	}
 }
